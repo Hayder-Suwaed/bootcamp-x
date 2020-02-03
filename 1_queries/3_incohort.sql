d85d7d978053828b50b4283a0c867116a21067ab
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-02-03 23:15:58 +0000

    select students who have not added email or phone to thier account

--- 1_queries/3_incohort.sql
+++ 1_queries/3_incohort.sql
@@ -1,9 +1,11 @@
 SELECT name, id
-cohort_id FROM students
+cohort_id
+FROM students
 WHERE email IS null
-OR phone IS null;
+    OR phone IS null;
 
 SELECT name, id
-cohort_id FROM students
+cohort_id
+FROM students
 WHERE email IS null
-OR phone IS null;
+    OR phone IS null;
