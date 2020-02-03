d6ee590c8014e5336614678c57bf38262bc414b2
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-02-03 22:19:36 +0000

    create tables for cohorts and students

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -1,4 +1,5 @@
-CREATE TABLE teachers (
+CREATE TABLE teachers
+(
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   is_active BOOLEAN DEFAULT TRUE,
@@ -6,7 +7,8 @@ CREATE TABLE teachers (
   end_date DATE
 );
 
-CREATE TABLE assistance_requests (
+CREATE TABLE assistance_requests
+(
   id SERIAL PRIMARY KEY NOT NULL,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
