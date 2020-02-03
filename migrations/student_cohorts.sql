ed6a6980046a664c0f5b40317f3d8a1c6f700ca3
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-02-03 22:21:16 +0000

    create table and database

--- migrations/student_cohorts.sql
+++ migrations/student_cohorts.sql
@@ -1,17 +1,19 @@
-CREATE TABLE cohorts (
+CREATE TABLE cohorts
+(
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   start_date DATE,
   end_date DATE
-  );
+);
 
-  CREATE TABLE students (
-    id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    phone VARCHAR(32),
-    github VARCHAR(255),
-    start_date DATE,
-    end_date DATE,
-    cohort_id INTEGER REFERENCES cohort(id) ON DELETE CASCADE
-  );
+CREATE TABLE students
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone VARCHAR(32),
+  github VARCHAR(255),
+  start_date DATE,
+  end_date DATE,
+  cohort_id INTEGER REFERENCES cohort(id) ON DELETE CASCADE
+);
