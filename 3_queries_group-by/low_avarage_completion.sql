0cd55dc7f4baad153f6e98d9a94b658b6e21a8e9
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-02-04 08:55:58 +0000

    Low Average Completion Time

--- 3_queries_group-by/low_avarage_completion.sql
+++ 3_queries_group-by/low_avarage_completion.sql
@@ -4,4 +4,4 @@ SELECT (
 )-count(assignment_submissions) as total_incomplete
 FROM assignment_submissions
 JOIN students ON students.id = student_id
-WHERE students.name = 'Ibrahim Schimmel';
+WHERE students.name = 'Ibrahim Schimmel'; 
