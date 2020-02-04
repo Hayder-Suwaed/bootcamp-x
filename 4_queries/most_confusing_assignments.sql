2d972b81242c86693bd239884e4baa8837b095d7
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-02-04 21:49:08 +0000

    Most Confusing Assignments

--- 4_queries/most_confusing_assignments.sql
+++ 4_queries/most_confusing_assignments.sql
@@ -1,5 +1,5 @@
 SELECT assignments.id, name, day, chapter, COUNT(assistance_requests) AS total_requests
   FROM assistance
   JOIN assistance_requests ON assignments.id = assignment_id
-  GROUP BY assignments.id
-  ORDER BY total_requests DESC;
+  GROUP BY assignments.id 
+  ORDER BY total_requests DESC; 
