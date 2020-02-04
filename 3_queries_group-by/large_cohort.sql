cc0796f54c382515530070ea6191d78a311c9f34
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-02-04 08:55:06 +0000

    large cohort

--- 3_queries_group-by/large_cohort.sql
+++ 3_queries_group-by/large_cohort.sql
@@ -2,4 +2,4 @@ SELECT cohorts.name, count(*) AS student_count FROM students
 JOIN cohorts ON cohort_id = cohorts.id
 GROUP BY cohorts.name
 HAVING count(*) >= 18
-ORDER BY count(*);
+ORDER BY count(*); 
