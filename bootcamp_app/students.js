e2bfe080ae7de3a6f5901e6c289436b5c608a347
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-02-06 06:42:46 +0000

    implement pool and client for student file

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,21 +1,24 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'bootcampx'
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx"
 });
 
-
-
-pool.query(`
+pool
+  .query(
+    `
 SELECT id, name, cohort_id
 FROM students
 LIMIT 5;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
-  })
-});
+`
+  )
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`
+      );
+    });
+  });
